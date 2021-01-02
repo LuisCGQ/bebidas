428de7ff8541debde1132608b8e9983b9c821497
Luis García <aig.luis@gmail.com>
2021-01-02 17:30:58 -0600

    agregado useContext para las categorias

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+
+import CategoriasProvider from "./context/CategoriasContext";
+import RecetasProvider from "./context/RecetasContext";
 
 function App() {
-  return <h1>Bebidas</h1>;
+  return (
+    <CategoriasProvider>
+      <RecetasProvider>
+        <Header />
+        <div className="container mt-5">
+          <div className="row">
+            <Formulario />
+          </div>
+        </div>
+      </RecetasProvider>
+    </CategoriasProvider>
+  );
 }
 
 export default App;
